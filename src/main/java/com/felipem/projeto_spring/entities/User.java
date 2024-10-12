498d8d3861a2ccf685a505b73e12c7e7fd315014
mfelipe758 <almeida.felipejs@gmail.com>
2024-10-12 11:21:38 -0300

    parte 1 Order, Instant, ISO 8601

--- src/main/java/com/felipem/projeto_spring/entities/User.java
+++ src/main/java/com/felipem/projeto_spring/entities/User.java
@@ -3,9 +3,11 @@ package com.felipem.projeto_spring.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 @Entity
-@Table(name = "tb_user ")
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -16,6 +18,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User(){
     }
     public User(Long id, String name, String email, String phone, String password) {
@@ -79,4 +84,8 @@ public class User implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
+    public List<Order> getOders() {
+        return orders;
+    }
 }
+
